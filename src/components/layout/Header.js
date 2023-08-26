b881f774824ab005dd011fa1ed9bf8fd59520d51
dinesh subhash pawar <dspfamily8@gmail.com>
2023-08-26 20:22:51 +0530

    j

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -80,7 +80,7 @@ const Header = () => {
         <div className="nav-right">
           <ul>
             <li>
-              <a href="#home">Home</a>
+              {/* <a href="#home">Home</a> */}
             </li>{" "}
             <li>
               <a href="#services">Services</a>
